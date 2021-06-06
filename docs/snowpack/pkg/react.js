95e43b344ed6562142669d83c09db6e0af47b677
Croustys <akos.barabas2002@gmail.com>
2021-06-06 23:44:06 +0200

    deleted docs

--- docs/snowpack/pkg/react.js
+++
@@ -1,9 +0,0 @@
-import { r as react } from './common/index-4b0af3e2.js';
-export { r as default } from './common/index-4b0af3e2.js';
-
-
-
-var createContext = react.createContext;
-var useContext = react.useContext;
-var useState = react.useState;
-export { createContext, useContext, useState };
